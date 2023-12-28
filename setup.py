b7c5becc8fe5228cef090319e10393ec09d156fb
Grega Krajnc <grega.krajnc@srnjak.com>
2023-12-29 00:32:30 +0100

    Preparation for deploying to pypi repository.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gate-controller',
+    version='1.0.0',
+    author='Grega Krajnc',
+    author_email='grega.dev@srnjak.com',
+    description='The Gate Controller Project, specifically designed for the '
+                'Raspberry Pi.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/srnjak/gate-controller',
+    packages=find_packages(),  # Automatically find packages
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Home Automation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'Flask',
+        'PyYAML',
+    ],
+    extras_require={
+        'RPi': ['RPi.GPIO'],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['*.yaml', '*.json'],
+        'gate_controller': ['static/*.yaml'],
+    },
+)
