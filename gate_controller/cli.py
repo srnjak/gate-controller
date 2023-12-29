944ddb63a5c28c82917d622782270d55366987f7
Grega Krajnc <grega.krajnc@srnjak.com>
2023-12-29 14:47:26 +0100

    Added cli, updated readme.

---
+++ gate_controller/cli.py
@@ -0,0 +1,20 @@
+import sys
+from gate_controller.gate_controller import control_gate
+
+
+def cli():
+    valid_actions = ['open', 'close', 'stop', 'toggle']
+
+    if len(sys.argv) != 2 or sys.argv[1] not in valid_actions:
+        print("Usage: gate-controller <action>")
+        print("Actions:")
+        for action in valid_actions:
+            print(f"  {action}")
+        sys.exit(1)
+
+    action = sys.argv[1]
+    control_gate(action)
+
+
+if __name__ == "__main__":
+    cli()
